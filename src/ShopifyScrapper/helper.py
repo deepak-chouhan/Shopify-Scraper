cbd2b075d494b583bbfa79f0c7d5c3bce44b3bed
deepak <deepakceo0@gmail.com>
2022-07-24 11:18:14 +0530

    Breaking Data into page Finished

--- src/ShopifyScrapper/helper.py
+++ src/ShopifyScrapper/helper.py
@@ -1,3 +1,4 @@
+import requests
 from .models import *
 
 # Helper Class for Saving Product
@@ -43,8 +44,8 @@ class Product:
                     variant_id = variant["id"],
                     product = self.Data_obj,
                     title = variant["title"],
-                    sku = variant["sku"] or None,
-                    featured_image = variant["featured_image"]["src"] or None,
+                    sku = variant["sku"] or "null",
+                    featured_image = variant["featured_image"]["src"] if variant["featured_image"] != None else None,
                     price = float(variant["price"]),
                     created_at = variant["created_at"],
                     updated_at = variant["updated_at"]
@@ -62,3 +63,33 @@ class Product:
                     product = self.Data_obj
                 )
                 image_obj.save()
+
+def Scrape(urls):
+    """
+    Scrape Data for given URL
+
+    Args:
+        urls (string): Shopify Store Page URLS
+    """
+    for url in urls:
+        # TODO: Logic to restart scrapping every 5 min
+        
+        store_url = url.url + "/products.json?limit=250&page="
+        page_no = 1
+        has_data = True
+        while(has_data):
+            page_url = store_url + str(page_no)
+            req = requests.get(page_url)
+            data = req.json()
+
+            if(len(data["products"]) > 0):
+                for product in data["products"]:
+                    do_exist = Data.objects.filter(product_id=product["id"]).exists()
+                    if do_exist == False:
+                        product_obj = Product(product, url.website)
+                        product_obj.Data()
+                        product_obj.Variants()
+                        product_obj.ProductImages()
+            else:
+                has_data = False
+            page_no += 1
--- src/ShopifyScrapper/views.py
+++ src/ShopifyScrapper/views.py
@@ -1,25 +1,9 @@
-import json
-from django.http import HttpResponse, JsonResponse
-from django.core import serializers
-import requests
-import bs4
+from tkinter import Scale
+from django.http import HttpResponse
 from .models import *
-from .helper import Product
+from .helper import Scrape
 
 def getData(request):
     urls = Url.objects.filter(website__status=True)
-
-    for url in urls:
-
-        # TODO: Logic to break the data into pages
-        store_url = url.url + "/products.json?limit=1"
-        req = requests.get(store_url)
-        data = req.json()
-        for product in data["products"]:
-            do_exist = Data.objects.filter(product_id=product["id"]).exists()
-            if do_exist == False:
-                product_obj = Product(product, url.website)
-                product_obj.Data()
-                product_obj.Variants()
-                product_obj.ProductImages()
+    Scrape(urls)
     return HttpResponse(urls)
